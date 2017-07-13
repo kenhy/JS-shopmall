1abefa5ab887127a9be9614ec8e630883b487eaf
Jonathan Zhang <zhangjianan1996@qq.com>
2017-07-13 10:04:42 +0800

    update home
    home

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
 /**
  * Created by JonathanZhang on 2017/7/11.
  */
+
+function init_index_all() {
+    init_index.initBanner();
+    var data = init_index_detail();
+    console.log(data);
+    index_footer_model(data);
+    index_footer_superDeal(data);
+}
+
 var init_index = {
     //初始化首页轮播图
     initBanner: function () {
@@ -21,26 +30,45 @@ var init_index = {
 
             }
         });
-    },
-    //初始化首页底部
-    initFooter:function () {
-        $.ajax({
-            type:"get",
-            url:RTTMALL_API.URL_INDEX_FOOTER,
-            dataType:"json",
-            async:false,
-            cache:false,
-            data:{},
-            success:function (data) {
-                console.log(data);
-            }
-        })
     }
+
 };
 
-
-function init_index_detail(data) {
-
+//初始化首页底部
+function init_index_detail() {
+    var index;
+    $.ajax({
+        type:"get",
+        url:RTTMALL_API.URL_INDEX_FOOTER,
+        dataType:"json",
+        async:false,
+        cache:false,
+        data:{},
+        success:function (data) {
+            index = data.data;
+        }
+    });
+    return index;
 }
+//model
+function index_footer_model(data) {
+    var index = data.models;
+    //console.log(index);
+    /*1*/
+    var list1 = template('index-footer-Sellers',index[0]);
+    //console.log(list);
+    $("[data-type=index-footer-Sellers]").html(list1);
+    /*2*/
+    var list2 = template('index-footer-Arrivals',index[1]);
+    console.log(list2);
+    $("[data-type=index-footer-Arrivals]").html(list2);
+}
+//superDeal
+function index_footer_superDeal(data) {
+    var index = data.superDeal;
+    //console.log(index);
+}
+
+
 
 
