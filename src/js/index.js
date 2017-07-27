764987e0dae671608f3262e773bc02293ffadd1c
Jonathan Zhang <zhangjianan1996@qq.com>
2017-07-27 10:47:20 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,17 @@
 /**
  * Created by JonathanZhang on 2017/7/11.
  */
-
-
-function init_index_all() {
+$(document).ready(function () {
     init_index.initBanner();
     var data = init_index_detail();
     //console.log(data);
     index_footer_model(data);
-    index_footer_superDeal(data);
+    initSuperDeals(data);
     login_status();
-}
+    initSuperDeals();
+    timer();
+});
+
 
 var init_index = {
     //初始化首页轮播图
@@ -19,7 +20,7 @@ var init_index = {
             type: "get",
             url: RTTMALL_API.URL_INDEX_BANNER,
             dataType: "json",
-            async: false,
+            async: true,
             cache: false,
             data: {
                 client: "ios"
@@ -33,20 +34,19 @@ var init_index = {
             }
         });
     }
-
 };
 
 //初始化首页底部
 function init_index_detail() {
     var index;
     $.ajax({
-        type:"get",
-        url:RTTMALL_API.URL_INDEX_FOOTER,
-        dataType:"json",
-        async:false,
-        cache:false,
-        data:{},
-        success:function (data) {
+        type: "get",
+        url: RTTMALL_API.URL_INDEX_FOOTER,
+        dataType: "json",
+        async: false,
+        cache: false,
+        data: {},
+        success: function (data) {
             index = data.data;
             //console.log(data);
         }
@@ -57,96 +57,31 @@ function init_index_detail() {
 //model
 function index_footer_model(data) {
     var index = data.models;
-    console.log(index);
+    //console.log(index);
     /*1*/
-    var list1 = template('index-footer-Sellers',index[0]);
+    var list1 = template('index-footer-Sellers', index[0]);
     //console.log(list);
     $("[data-type=index-footer-Sellers]").html(list1);
     /*2*/
-    var list2 = template('index-footer-Arrivals',index[1]);
+    var list2 = template('index-footer-Arrivals', index[1]);
     //console.log(list2);
     $("[data-type=index-footer-Arrivals]").html(list2);
 }
 
-
-
-//superDeal
-function index_footer_superDeal(data) {
-    var index = data.superDeal;
-    console.log(index);
-}
-
-//login 状态
-function login_status() {
-    var token = getCookie("token"),
-        username = getCookie("userName"),
-        loginName = getCookie("loginName"),
-        imagePath = getCookie("imagePath"),
-        user_login = $('#user_login'),
-        index_chart = $('#index_chart'),
-        tpl;
-        //console.log(username);
-
-    if (token == ""){
-    //console.log(username);
-    //console.log(imagePath);
-        href_check();
-        tpl = '<img class="mui-media-object mui-pull-left radius50" src="images/Default-Avatar.jpg">'+
-            '<div class="mui-media-body margin-t20">'+
-            '<p class="font14 font-weight font-color-3">'+
-            '<a class="font16 font-color-9" href="containers/login/login.html">Login in</a>'+
-            '<a class="font16" href="javascript:void(0)"> | </a>'+
-            '<a class="font16 font-color-9" href="containers/login/register.html">Register</a>'+
-            '</p>'+
-            '</div>';
-        //console.log(tpl);
-        user_login.html(null);
-        user_login.html(tpl);
-
-        index_chart.attr("href","containers/login/login.html");
-    }else{
-        if(imagePath == "null"){
-            imagePath = "images/Default-Avatar.jpg";
-        }
-        tpl = '<a href="containers/mine/account.html">'+
-            '<img class="mui-media-object mui-pull-left radius50" src="'+ imagePath +'">'+
-            '<div class="mui-media-body">'+
-            '<p class="font14 font-weight font-color-3">'+ username +'</p>'+
-            '<p class="mui-ellipsis font-color-6 font12">'+ loginName +'</p>'+
-            '<p class="iconfont icon-vip ">'+
-            '<span class="font10 font-color-9">Membership</span>'+
-            '</p>'+
-            '</div>'+
-            '<s class="mui-navigate-right font18"></s>'+
-            '</a>';
-        //console.log(tpl);
-        user_login.html(null);
-        user_login.html(tpl);
-        index_chart.attr("href","containers/shoppingcard/shopping-cart.html");
-    }
-}
+var timeout = false; // 启动及关闭按钮
+var timerEndTime;
 
 function timer() {
-    countdown();
+
+    if (timeout) {
+        show_date_time()
+    }
     setTimeout(timer, 1000); // time是指本身,延时递归调用自己,100为间隔调用时间,单位毫秒
 }
 
-function countdown() {
-    $("[data-type=countdown]").each(function(index) {
-        var endTime = $(this).attr("data-countdown");
-        time = show_date_time(endTime);
-        if (time == "close") {
-            $(this).attr("data-type", "close");
-            $(this).html("Close");
-        } else {
-            $(this).html(time);
-        }
-    });
-}
-
-function show_date_time(timerEndTime) {
+function show_date_time() {
     // 计算目标时间与当前时间间隔(毫秒数)
-    var timeold = timerEndTime;
+    var timeold = timerEndTime - new Date().getTime();
     // 得到小时数
     var e_hrsold = timeold / (60 * 60 * 1000);
     // 得到小时数(整数)
@@ -158,15 +93,103 @@ function show_date_time(timerEndTime) {
     // 得到秒数(整数)
     seconds = Math.floor((e_minsold - minsold) * 60);
     if (hrsold < 0) {
-        return "close";
+        timeout = false;
+        initSuperDeals();
     } else {
-        return formatTime(hrsold) + ":" + formatTime(minsold) + ":"
-            + formatTime(seconds);
+        $("[data-role=hour]").html(formatTime(hrsold));
+        $("[data-role=minute]").html(formatTime(minsold));
+        $("[data-role=second]").html(formatTime(seconds));
     }
 }
 
-function formatTime(timeStr) {
-    return timeStr < 10 ? "0" + timeStr : timeStr;
+function initSuperDeals() {
+    $.ajax({
+        type: "get",
+        url: RTTMALL_API.URL_INDEX_SUPERDEALS,
+        dataType: "json",
+        async: true,
+        cache: false,
+        data: {},
+        success: function (data) {
+            if (data != null) {
+                console.log(data);
+                if (data.data.productList != null && data.data.productList.length > 0) {
+                    if (data.data.isNow) {
+                        $("#deit").text("Deal ends in:");
+                        startTimer(data.data.endTime);
+                    } else {
+                        $("#deit").text("New deal starts in:");
+                        startTimer(data.data.beginTime);
+                    }
+                    var html = template('superDealsTemplate', data.data);
+                    //$("[data-type=superDealsMain]").html(html);
+                }
+            }
+        }
+    });
 }
+
+function startTimer(endTime) {
+    timerEndTime = endTime;
+    timeout = true;
+}
+
+function formatTime(timeStr) {
+    if (timeStr < 10)
+        timeStr = "0" + timeStr;
+    return timeStr;
+}
+
+//login 状态
+function login_status() {
+    var token = getCookie("token"),
+        username = getCookie("userName"),
+        loginName = getCookie("loginName"),
+        imagePath = getCookie("imagePath"),
+        user_login = $('#user_login'),
+        index_chart = $('#index_chart'),
+        tpl;
+    //console.log(username);
+
+    if (token == "") {
+        //console.log(username);
+        //console.log(imagePath);
+        setCookie("login_status", "false");
+        href_check();
+        tpl = '<img class="mui-media-object mui-pull-left radius50" src="images/Default-Avatar.jpg">' +
+            '<div class="mui-media-body margin-t20">' +
+            '<p class="font14 font-weight font-color-3">' +
+            '<a class="font16 font-color-9" href="containers/login/login.html">Login in</a>' +
+            '<a class="font16" href="javascript:void(0)"> | </a>' +
+            '<a class="font16 font-color-9" href="containers/login/register.html">Register</a>' +
+            '</p>' +
+            '</div>';
+        //console.log(tpl);
+        user_login.html(null);
+        user_login.html(tpl);
+
+        index_chart.attr("href", "containers/login/login.html");
+    } else {
+        if (imagePath == "null") {
+            imagePath = "images/Default-Avatar.jpg";
+        }
+        tpl = '<a href="containers/mine/account.html">' +
+            '<img class="mui-media-object mui-pull-left radius50" src="' + imagePath + '">' +
+            '<div class="mui-media-body">' +
+            '<p class="font14 font-weight font-color-3">' + username + '</p>' +
+            '<p class="mui-ellipsis font-color-6 font12">' + loginName + '</p>' +
+            '<p class="iconfont icon-vip ">' +
+            '<span class="font10 font-color-9">Membership</span>' +
+            '</p>' +
+            '</div>' +
+            '<s class="mui-navigate-right font18"></s>' +
+            '</a>';
+        //console.log(tpl);
+        user_login.html(null);
+        user_login.html(tpl);
+        index_chart.attr("href", "containers/shoppingcard/shopping-cart.html");
+    }
+}
+
 
 
